94e3b1a2451720ead2a9a3b7b2a601aed49edba9
Guillermo Alfaro <galfarotolon@gmail.com>
2020-05-15 13:08:58 -0500

    mvp

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
+
